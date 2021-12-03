1621ada66a099f156d4e2b90e318cb312df75f69
Karolin Kopper <k.kopper@campus.lmu.de>
2021-12-03 12:55:03 +0100

    add swipe funciton

--- app/src/main/java/com/example/eveningswipe/MainActivity.kt
+++ app/src/main/java/com/example/eveningswipe/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
