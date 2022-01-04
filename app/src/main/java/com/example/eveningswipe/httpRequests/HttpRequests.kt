66d31870d163b979cc9f1205473ebd985aed9f2c
helena564 <helena.stoll98@gmx.de>
2022-01-04 16:31:29 +0100

    new requests

--- app/src/main/java/com/example/eveningswipe/httpRequests/HttpRequests.kt
+++ app/src/main/java/com/example/eveningswipe/httpRequests/HttpRequests.kt
@@ -1,9 +1,12 @@
 package com.example.eveningswipe.httpRequests
 
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 
 object HttpRequests {
     private var Response = ArrayList<FilterByGroupId>()
+    private var ResponseFilterRating = ArrayList<FilterRating>()
+    private var ResponseCreateGroup = ArrayList<CreateGroup>()
 
     fun getMovieById(url: String): ArrayList<FilterByGroupId> {
         url.httpGet().responseObject(FilterByGroupId.Deserializer()) { request, response, result ->
@@ -15,4 +18,27 @@ object HttpRequests {
         }
         return Response
     }
+
+    fun getFilterRating(url: String): ArrayList<FilterRating> {
+        url.httpGet().responseObject(FilterRating.Deserializer()) { request, response, result ->
+            val (item, err) = result
+
+            item?.forEach { element ->
+                ResponseFilterRating.add(element)
+            }
+        }
+        return ResponseFilterRating
+    }
+
+    fun postCreatedGroup(url: String): ArrayList<CreateGroup> {
+        url.httpPost().responseObject(CreateGroup.Deserializer()) { request, response, result ->
+            val (item, err) = result
+
+            item?.forEach { element ->
+                ResponseCreateGroup.add(element)
+            }
+        }
+        return ResponseCreateGroup
+    }
+
 }
