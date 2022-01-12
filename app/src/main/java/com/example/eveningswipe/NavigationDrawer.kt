a0fef9da2d562fe402a4e16a4170a30c6b9754d8
helena564 <helena.stoll98@gmx.de>
2022-01-12 20:46:11 +0100

    logout

--- app/src/main/java/com/example/eveningswipe/NavigationDrawer.kt
+++ app/src/main/java/com/example/eveningswipe/NavigationDrawer.kt
@@ -1,18 +1,16 @@
 package com.example.eveningswipe
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.ui.*
 
 class NavigationDrawer : AppCompatActivity() {
 
@@ -36,12 +34,24 @@ class NavigationDrawer : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_swipe, R.id.nav_logout
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setNavigationItemSelectedListener {dest ->
+            when(dest.itemId) {
+                R.id.nav_logout -> logout()
+                else -> {
+                    NavigationUI.onNavDestinationSelected(dest, navController)
+                    drawerLayout.closeDrawers()
+                }
+            }
+            true
+        }
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -52,5 +62,11 @@ class NavigationDrawer : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun logout() {
+        val profileIntent = Intent(this@NavigationDrawer, MainActivity::class.java)
+        startActivity(profileIntent)
+        finish()
     }
 }
