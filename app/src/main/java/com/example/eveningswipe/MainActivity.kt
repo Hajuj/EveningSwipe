d34a74acd307e0cdbf6baea9657decd5367f6b32
Mohamad Hajuj <hajuj_m7md@hotmail.com>
2022-01-29 00:38:24 +0100

    added splash screen

--- app/src/main/java/com/example/eveningswipe/MainActivity.kt
+++ app/src/main/java/com/example/eveningswipe/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_EveningSwipe)
         setContentView(R.layout.activity_main)
 
         //for dark mode
