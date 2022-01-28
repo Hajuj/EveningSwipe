2420e35581924b2b8dbc29c6baffc19be05b5e45
yannick <yannick.j.weber@hotmail.de>
2022-01-28 19:08:58 +0100

    repaired Register, added negative rating to rateMovie, added getSwipestate

---
+++ user_demo_postgres/src/main/kotlin/com/example/user_demo_postgres/returnDto/RetSwipeStateDto.kt
@@ -0,0 +1,5 @@
+package com.example.user_demo_postgres.returnDto
+
+data class RetSwipeStateDto (
+    val nextMovie: Int
+)
---
+++ user_demo_postgres/src/main/kotlin/com/example/user_demo_postgres/returnDto/RetmesageDto.kt
@@ -0,0 +1,5 @@
+package com.example.user_demo_postgres.returnDto
+
+data class RetmesageDto(
+    val message: String
+)
